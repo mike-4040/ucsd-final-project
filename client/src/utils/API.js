8ce3747748daf61037150876222ee31a64bc334d
Mike Kravtsov <mike4040@me.com>
2019-11-05 14:04:26 -0800

    Fixed rentee page

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -17,10 +17,16 @@ export default {
   newRequest: newRequest => {
     return axios.post("/api/request/", newRequest);
   },
-  // Get an array of all requests of the Rentee
-  getRenteeReqs: renteeId => axios.get(`/api/requests/${renteeId}`),
+
+  // Get an array of all OPEN requests of the Rentee
+  getRenteeReqsOpen: renteeId => axios.get(`/api/requestsO/${renteeId}`),
+
+  // Get an array of all CLOSED requests of the Rentee
+  getRenteeReqsClosed: renteeId => axios.get(`/api/requestsC/${renteeId}`),
+
   //Get an array of one request of Rentee
   getSingleRequest: requestId => axios.get(`/api/request/${requestId}`),
+  
   //Getting all offers on single requestID
   getAllOffers: requestId => axios.get(`/api/offers/${requestId}`),
 
