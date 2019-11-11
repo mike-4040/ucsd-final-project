e4c7ff49ad826a5afc497fb1b4fa713396b63d94
konstagap <konstagapov123@gmail.com>
2019-11-10 16:37:02 -0800

    edited date picker, edited alerts on dublicate email

--- client/src/pages/NewRequest.js
+++ client/src/pages/NewRequest.js
@@ -2,8 +2,7 @@ import React, { Component } from "react";
 import withAuth from "./../components/withAuth";
 import API from "./../utils/API";
 import { Link } from "react-router-dom";
-import DateTimePicker from 'react-datetime-picker';
-
+import DateTimePicker from "react-datetime-picker";
 
 class NewRequest extends Component {
   state = {
@@ -12,7 +11,7 @@ class NewRequest extends Component {
     time: "",
     location: ""
   };
-  onChangeTime = time => this.setState({ time })
+  // onChangeTime = time => this.setState({ time })
 
   handleChange = event => {
     const { name, value } = event.target;
@@ -30,13 +29,38 @@ class NewRequest extends Component {
       location: this.state.location,
       time: this.state.time
     };
-    API
-      .newRequest(newRequest)
-      .then(res => this.props.history.push('/rentee/'))
+    API.newRequest(newRequest)
+      .then(res => this.props.history.push("/rentee/"))
       .catch(err => alert(err));
   };
 
+  getCurrentTime = () => {
+    let now = new Date();
+    let utcString = now.toISOString().substring(0, 19);
+    let year = now.getFullYear();
+    let month = now.getMonth() + 1;
+    let day = now.getDate();
+    let hour = now.getHours();
+    let minute = now.getMinutes();
+    // let second = now.getSeconds();
+    let localDatetime =
+      year +
+      "-" +
+      (month < 10 ? "0" + month.toString() : month) +
+      "-" +
+      (day < 10 ? "0" + day.toString() : day) +
+      "T" +
+      (hour < 10 ? "0" + hour.toString() : hour) +
+      ":" +
+      (minute < 10 ? "0" + minute.toString() : minute) +
+      utcString.substring(16, 19);
+    this.setState({
+      time: localDatetime
+    });
+  };
   componentDidMount() {
+    this.getCurrentTime()
+    console.log(new Date().getMinutes());
     API.getUser(this.props.user.id).then(res => {
       this.setState({
         username: res.data.username
@@ -48,7 +72,7 @@ class NewRequest extends Component {
     return (
       <div className="container ">
         <div className="row justify-content-center">
-        <div className="col-sm-11  col-md-8 col-lg-7 wrapper">
+          <div className="col-sm-11  col-md-8 col-lg-7 wrapper">
             <br />
             <h1>Create new request</h1>
             <br />
@@ -97,13 +121,13 @@ class NewRequest extends Component {
                   Time
                 </label>
                 <div className="col-sm-10">
-                <DateTimePicker
+                  {/* <DateTimePicker
                   onChange={this.onChangeTime}
                   value={this.state.time}
                     disableClock="true"
                     className="form-control"
-                />
-                  {/* <input
+                /> */}
+                  <input
                     value={this.state.time}
                     onChange={this.handleChange}
                     type="datetime-local"
@@ -111,9 +135,8 @@ class NewRequest extends Component {
                     className="form-control"
                     name="time"
                     id="time"
-                    placeholder=""
                     required
-                  /> */}
+                  />
                 </div>
               </div>
               <div className="form-group row">
